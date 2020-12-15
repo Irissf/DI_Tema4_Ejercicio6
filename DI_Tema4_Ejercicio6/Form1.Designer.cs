439c8b10438b8d5a9973c7c23e33b4a319e3076e
Irissf <iris.seijo@gmail.com>
2020-12-15 14:48:06 +0100

    avanzado un rato más

--- DI_Tema4_Ejercicio6/Form1.Designer.cs
+++ DI_Tema4_Ejercicio6/Form1.Designer.cs
@@ -28,32 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel1.Location = new System.Drawing.Point(12, 136);
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(251, 302);
-            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonClick);
+            this.button1.MouseEnter += new System.EventHandler(this.BUttonCursorEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.ButtonCursorLeave);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(276, 449);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GenerateButtons);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- DI_Tema4_Ejercicio6/Form1.cs
+++ DI_Tema4_Ejercicio6/Form1.cs
@@ -12,9 +12,72 @@ namespace DI_Tema4_Ejercicio6
 {
     public partial class Form1 : Form
     {
+        public Button btn;
+        public string numbers = "123456789*0#";
+        public int count = 0;
+        public int x = 15;
+        public int y = 15;
+
+        //==========================================================================================
+
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = Color.FromArgb(127, 120, 121);
+            this.Text = "Contacts";
+        }
+
+        private void GenerateButtons(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    btn = new System.Windows.Forms.Button();
+                    btn.Width = 50;
+                    btn.Height = 50;
+                    btn.Location = new Point(x, y);
+                    btn.Text = "" + numbers[count];
+                    btn.FlatStyle = FlatStyle.Standard;
+                    btn.Font = new System.Drawing.Font("Console", 10, FontStyle.Regular);
+                    btn.BackColor = Color.FromArgb(238, 232, 226);
+                    btn.MouseEnter += new System.EventHandler(this.BUttonCursorEnter);
+                    btn.MouseLeave += new System.EventHandler(this.ButtonCursorLeave);
+                    btn.Click += new System.EventHandler(this.ButtonClick);
+                    this.panel1.Controls.Add(btn);
+                    if (j == 2)
+                    {
+                        x = 15;
+                    }
+                    else
+                    {
+                        x = x + 50 + 15;
+                    }
+                    count++;
+                }
+                y = y + 50 + 15;
+            }
+
+        }
+
+        private void BUttonCursorEnter(object sender, EventArgs e)
+        {
+            Button btnEnter = (Button)sender;
+            btnEnter.BackColor = Color.FromArgb(232, 212, 205);
+           
+        }
+
+        private void ButtonCursorLeave(object sender, EventArgs e)
+        {
+            Button btnLeave = (Button)sender;
+           
+                btnLeave.BackColor = Color.FromArgb(238, 232, 226);
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button btnClick = (Button)sender;
+            btnClick.BackColor = Color.FromArgb(217, 195, 189);
         }
     }
 }
